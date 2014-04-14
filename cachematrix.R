306322741c824d7e6e2bc078ab53bbfc0d2af6ac
Stefan Baerisch <stefan.baerisch@gmail.com>
2014-04-14 07:52:08 +0200

    Solution written, readme changed

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,53 @@
-## Put comments here that give an overall description of what your
-## functions do
+##
+## makeCacheMatrix() : create a matrix 'object' with setter and getter
+##                     functions for matrix and cache value
+## cacheSolve()      : get the inverse for a matrix created by 
+##                    makeCacheMatrix(), either from cache or by
+##                    calculating the results
+## testCache()       : simple test function
 
-## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
 
+
+# Build a cache object with 4 methods to get/set raw values and results
+# Used by cacheSolve() function
+makeCacheMatrix <- function(c_m = matrix()) {
+  c_inv <- NULL
+  set <- function(m) {
+    c_m <<- m
+    c_inv <<- NULL
+  }
+  get <- function() { c_m }
+  setsolved <- function(inv) {c_inv <<- inv}
+  getsolved <- function() { c_inv }
+  list(set = set, get = get,
+       setsolved = setsolved,
+       getsolved = getsolved)
 }
 
+# Returns the inverse of a matrix object created by makeCacheMatrix()
+# Checks for the cached value and returns it, if present, otherwise
+# calculates the value and returns it
+cacheSolve <- function(cache_m, ...) {
+  # Get value or NULL from cache
+  inv <- cache_m$getsolved()
+  if(!is.null(inv)) {
+    #Cache set, return data
+    message("getting cached data")
+    return(inv)
+  }
+  #cache not set, solve and store value, then return
+  message("solving matrix")
+  mat <- cache_m$get()
+  inv <- solve(mat, ...)
+  cache_m$setsolved(inv)
+  inv
+}
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+#Testfunction for the cache 
+testCache <- function() {
+  m <- matrix(c(2,3,2,2),nrow=2)
+  cm <- makeCacheMatrix(m)
+  print(cacheSolve(cm))
+  print (cacheSolve(cm))
 }
